353f588e8f01e26284f3426ba99ae3c8454959a3
manuel-vl <manuel.valencia@mercadolibre.com.co>
2024-02-02 18:51:03 -0500

    Initial commit

--- src/exercise2/IImprimible.java
+++
@@ -1,9 +0,0 @@
-package exercise2;
-
-public interface IImprimible<T> {
-    default <T> void imprimir(T doc){
-        System.out.println(doc.toString());
-    }
-
-    void otraFormaImprimir();
-}
